dd6a7b9ae8be132e86a0e1e8cf3bcc02ea5a98fd
Muhammad <type.92@mail.ru>
2023-02-14 22:04:57 +0300

    END

---
+++ task11/Program.cs
@@ -0,0 +1,7 @@
+﻿int rand = new Random().Next(100, 1000);
+Console.WriteLine(rand);
+int valueA = rand / 100;
+int valueB = rand % 10;
+
+rand = valueA * 10 + valueB;
+Console.WriteLine(rand);
---
+++ task12/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+// int num1 = new  Random().Next(100,1000);
+// int num2 = new  Random().Next(1,100);
+
+if (num1 % num2 == 0)
+{
+    Console.WriteLine($"{num1} кратно {num2}");
+}
+else
+{
+
+    Console.WriteLine($"{num1} и {num2} некратны, остаток: {num1 % num2}");
+}
