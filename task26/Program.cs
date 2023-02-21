712a23284e3e2973a0d4e7484252c61253e71593
Muhammad <type.92@mail.ru>
2023-02-21 20:44:06 +0300

    END

---
+++ task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int inputNum()
+{
+    int count = 0;
+    Console.WriteLine("Input number");
+    string str = Convert.ToString(Console.ReadLine());
+    if (str.All(Char.IsDigit))
+    {
+        count = str.Length;
+    }
+    return count;
+}
+
+void show(int count)
+{
+    Console.WriteLine(count);
+}
+
+show(inputNum());
---
+++ task28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int Sum(int number)
+{
+    int sum = 1;
+    for(int i = 1; i <= number; i++)
+    {
+        sum*=i;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до N = {Sum(A)}");
