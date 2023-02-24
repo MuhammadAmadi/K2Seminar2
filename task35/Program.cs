657375481399e87282bf09085ca3a4d7eeb83504
Muhammad <type.92@mail.ru>
2023-02-24 22:27:39 +0300

    END

---
+++ task35/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+int Entry(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ArrSearch(int[] arry)
+{
+    int count = 0;
+    for(int i = 0; i < arry.Length; i++)
+    {
+        if(arry[i] > 9 && arry[i] < 100)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int[] ArrRandom(int lenght)
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,123);
+    }
+    return arr;
+}
+
+int lenght = Entry($"Введите длину массива");
+int[] arr = ArrRandom(lenght);
+Console.Write($"[{string.Join(", ", arr)}]");
+int count = ArrSearch(arr);
+Console.Write($" => {count}");
---
+++ task37/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+int Entry(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] Arr(int[] array)
+{
+    int[] arr;
+    if (array.Length % 2 == 0)
+    {
+        arr = new int[array.Length / 2];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = array[i] * array[array.Length - 1 - i];
+        }
+    }
+    else
+    {
+        arr = new int[array.Length / 2 + 1];
+
+        for (int i = 0; i < arr.Length-1; i++)
+        {
+            arr[i] = array[i] * array[array.Length - 1 - i];
+        }
+        arr[arr.Length-1] = array[arr.Length-1];
+    }
+
+
+    return arr;
+}
+
+int[] ArrRandom(int lenght)
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1,99);
+    }
+    return arr;
+}
+
+int lenght = Entry($"Введите длину массива");
+int[] arr = ArrRandom(lenght);
+Console.Write($"[{string.Join(", ", arr)}]");
+arr = Arr(arr);
+Console.Write($" => [{string.Join(", ", arr)}]");
