96a24ddf970e97b30219e9af81005b30c70d2436
Muhammad <type.92@mail.ru>
2023-02-28 22:19:53 +0300

    END

---
+++ task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int number = 0;
+string str = String.Empty;
+
+Console.WriteLine("Введите десятичное число");
+number = Convert.ToInt32(Console.ReadLine());
+
+while ( number > 0)
+{
+    str = Convert.ToString(number%2) + str;
+    number = number / 2;
+    
+}
+Console.WriteLine(str);
+
+
---
+++ task44/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int fib0 = 0, fib1 = 1, temp;
+string str = "0 1";
+for(int i = 2; i < N; i++)
+{
+    temp = fib0 + fib1;
+    fib0 = fib1;
+    fib1 = temp;
+    str = str + " " +Convert.ToString(temp);
+}
+Console.WriteLine(str);
